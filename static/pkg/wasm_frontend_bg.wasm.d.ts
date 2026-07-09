58f164f218a160a82aa45b170c6c82753c679c66
jimd <violindocgfx@gmail.com>
2026-07-09 06:15:58 -0600

    Add TypeScript definitions for WebAssembly module functions
    - Created a new TypeScript declaration file for the WebAssembly module `wasm_frontend_bg.wasm`.
    - Exported various functions including memory management and control settings.
    - Included function signatures for bindings and closures used in the WebAssembly context.

---
+++ static/pkg/wasm_frontend_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const set_face_weights: (a: number, b: number, c: number, d: number) => void;
+export const set_fov: (a: number) => void;
+export const start: () => void;
+export const set_doom_controls: (a: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h2875693fc9d71ee3: (a: number, b: number, c: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h50d09b5fc1d1768c: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h50d09b5fc1d1768c_2: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__hcf6c6fe45fc18838: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __wbindgen_start: () => void;
